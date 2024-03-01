061851f053b03145c9581a442ee151ffdfbe9773
with-heart <with.heart+git@pm.me>
2024-03-01 09:19:40 -0500

    refactor dev script

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,52 +1,22 @@
 import * as fs from 'node:fs/promises'
-import * as path from 'node:path'
-import pkg from '../package.json'
+import { getWorkspaces, type Workspace } from './workspace'
 
 buildWorkspaces()
 
 async function buildWorkspaces() {
-  for (const workspace of await getWorkspaces()) {
-    if (await isPrivateWorkspace(workspace)) continue
-
+  for (const workspace of await getWorkspaces(true)) {
     await buildWorkspace(workspace)
   }
 }
 
-async function buildWorkspace(workspace: string) {
-  const workspacePath = path.join(process.cwd(), workspace)
+async function buildWorkspace(workspace: Workspace) {
+  const { paths } = workspace
 
-  const dist = path.join(workspacePath, 'dist')
-  const source = path.relative(
-    dist,
-    path.join(workspacePath, 'src', 'index.ts'),
-  )
-  const output = `export * from '${source}'`
+  await fs.rm(paths.dist, { recursive: true })
+  await fs.mkdir(paths.dist)
 
-  const index = Bun.file(path.join(dist, 'index.js'))
-  const types = Bun.file(path.join(dist, 'index.d.ts'))
+  const output = `export * from '${paths.source}'`
 
-  await fs.rm(dist, { recursive: true })
-  await fs.mkdir(dist)
-
-  await Bun.write(index, output)
-  await Bun.write(types, output)
-}
-
-async function getWorkspaces() {
-  const workspaces = []
-
-  for (const workspace of pkg.workspaces) {
-    const glob = new Bun.Glob(workspace)
-
-    for await (const result of glob.scan({ onlyFiles: false })) {
-      workspaces.push(result)
-    }
-  }
-
-  return workspaces
-}
-
-async function isPrivateWorkspace(workspace: string): Promise<boolean> {
-  const pkg = await import(path.join(process.cwd(), workspace, 'package.json'))
-  return pkg.private === true
+  await Bun.write(paths.module, output)
+  await Bun.write(paths.types, output)
 }
